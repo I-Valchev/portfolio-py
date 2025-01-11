2ac75f7fc41d63433b117cc6baf9e08083c685a1
Ivo Valchev <Ivo.V.Valchev@gmail.com>
2025-01-11 20:57:17 +0200

    Print multiple portfolios at once

--- index.py
+++ index.py
@@ -10,17 +10,19 @@ from models.Portfolio import Portfolio
 
 # Parse command-line arguments
 parser = argparse.ArgumentParser(description="Investment portfolio tracker.")
-parser.add_argument('-p', '--portfolio', type=str, default="portfolio", help="Name of the portfolio folder (default is 'portfolio')")
+parser.add_argument('-p', '--portfolios', type=str, nargs='+', default=["portfolio"], help="Names of the portfolio folders (default is 'portfolio')")
 parser.add_argument('-s', '--summary', action='store_true', help="Include summary information")
 args = parser.parse_args()
 
-# Load the config
-config = Config(args)
+# Iterate over each portfolio and print the results
+for portfolio_name in args.portfolios:
+    # Load the config for the current portfolio
+    config = Config(argparse.Namespace(portfolio=portfolio_name, summary=args.summary))
 
-portfolio = Portfolio(config)
-periods = generateAllPeriods(datetime.date(2020, 1, 1), datetime.date.today())
+    portfolio = Portfolio(config)
+    periods = generateAllPeriods(datetime.date(2020, 1, 1), datetime.date.today())
 
-generator = TableGenerator(config)
-generator.setRows(periods, portfolio)
+    generator = TableGenerator(config)
+    generator.setRows(periods, portfolio)
 
-generator.print()
+    generator.print()
