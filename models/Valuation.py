527b2c6f7bdba4765af51e6ae18db47583320aa8
Ivo Valchev <Ivo.V.Valchev@gmail.com>
2025-01-12 09:49:05 +0200

    fix typo

--- models/Valuation.py
+++ models/Valuation.py
@@ -4,7 +4,7 @@ import decimal
 class Valuation:
     def __init__(self, date, value):
         self.date = date if type(date) is datetime.date else datetime.datetime.strptime(date, "%d.%m.%Y").date()
-        self.value = decimal.Decimal(eval(str(value))) .quantize(decimal.Decimal("0.00")) # Ensure value is a string before evaluating
+        self.value = decimal.Decimal(eval(str(value))).quantize(decimal.Decimal("0.00")) # Ensure value is a string before evaluating
 
     def __repr__(self):
         return self.date.strftime("%d.%m.%Y")+" : " + str(self.value)
